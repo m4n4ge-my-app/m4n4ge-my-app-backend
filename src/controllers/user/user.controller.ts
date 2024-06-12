0eaff2d5f80a55b7fc83d3f07e26aa6baa3e3be7
Dinyar <dinyar.dev@gmail.com>
2024-06-12 18:28:15 -0400

    prevent creation of a same user multiple times

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -2,11 +2,15 @@ import UserModel from '../../models/user.model'
 
 export const saveUser = async (userName: string, googleId: string) => {
   try {
-    const newUser = await UserModel.create({
-      userName,
-      googleId
-    })
-    return newUser
+    const user = await UserModel.findById(googleId).exec()
+    if (!user) {
+      const newUser = await UserModel.create({
+        userName,
+        googleId
+      })
+      return newUser
+    }
+    return user
   } catch (error) {
     console.error('Error while saving user:', error)
     return new Error('Error while saving user')
