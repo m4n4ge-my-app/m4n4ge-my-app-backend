586b42b2f94a69ffd7efcf39c8ebfa3093b68a30
Dinyar <dinyar.dev@gmail.com>
2024-06-12 21:22:36 -0400

    fix error

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -2,14 +2,16 @@ import UserModel from '../../models/user.model'
 
 export const saveUser = async (userName: string, googleId: string) => {
   try {
-    const user = await UserModel.findById(googleId).exec()
+    const user = await UserModel.findOne({ googleId }).exec()
     if (!user) {
       const newUser = await UserModel.create({
         userName,
         googleId
       })
+      console.log('User saved:', newUser)
       return newUser
     }
+    console.log('User already exist:', user)
     return user
   } catch (error) {
     console.error('Error while saving user:', error)
