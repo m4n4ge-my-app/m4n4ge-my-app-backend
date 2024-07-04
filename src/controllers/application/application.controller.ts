d19e2bd5fe97aaa869004a3b1eecf9dcc3d1802d
Dinyar <dinyar.dev@gmail.com>
2024-07-04 15:08:03 -0400

    add createApplication and getApplication endpoints

--- src/controllers/application/application.controller.ts
+++ src/controllers/application/application.controller.ts
@@ -9,3 +9,27 @@ export const getApplications: RequestHandler = async (_req, res, next) => {
     next(error)
   }
 }
+
+export const getApplication: RequestHandler = async (req, res, next) => {
+  const applicationId = req.params.id
+  try {
+    const application = await ApplicationModel.findById(applicationId).exec()
+    if (!application) {
+      res.status(404).json({ error: 'Application not found' })
+      return
+    }
+    res.status(200).json(application)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const createApplication: RequestHandler = async (req, res, next) => {
+  try {
+    const application = new ApplicationModel(req.body)
+    await application.save()
+    res.status(201).json(application)
+  } catch (error) {
+    next(error)
+  }
+}
--- src/routes/application/application.ts
+++ src/routes/application/application.ts
@@ -4,5 +4,7 @@ import * as applicationController from '../../controllers/application/applicatio
 const applicationsRouter = express.Router()
 
 applicationsRouter.get('/', applicationController.getApplications)
+applicationsRouter.get('/:id', applicationController.getApplication)
+applicationsRouter.post('/', applicationController.createApplication)
 
 export default applicationsRouter
