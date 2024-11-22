3912e304d535a4bef7b57f3b6218f80941d8083b
dinyar-dev <dinyar.dev@gmail.com>
2024-11-22 15:14:18 -0500

    requesting fileType from frontend so that files can be put into correct folders in s3 for better organization

--- src/controllers/documents/document.controller.ts
+++ src/controllers/documents/document.controller.ts
@@ -4,17 +4,36 @@ import { uploadToS3Bucket } from '../../util/s3'
 
 export const uploadToS3 = async (req: Request, res: Response) => {
   try {
-    // Ensure file is available
-    const file = req.file as Express.Multer.File // Multer attaches the file to `req.file`
-    if (!file) {
-      return res.status(400).json({ error: 'No file uploaded' })
+    const file = req.file as Express.Multer.File
+    const { fileType } = req.body
+
+    //ensure file and fileType are provided
+    if (!file || !fileType) {
+      return res.status(400).json({ error: 'File and fileType are required' })
     }
 
     //@ts-ignore
     const userId = req.user!._id
 
+    //for cases where fileType is provided but not recognized
+    let folder = 'uncategorized'
+
+    switch (fileType) {
+      case 'resume':
+        folder = 'resumes'
+        break
+      case 'coverLetter':
+        folder = 'coverLetters'
+        break
+      case 'jobDescription':
+        folder = 'jobDescriptions'
+        break
+      default:
+        break
+    }
+
     // Proceed with file upload to S3
-    const s3Result = await uploadToS3Bucket(file.buffer, `documents/${file.originalname}`, file.mimetype)
+    const s3Result = await uploadToS3Bucket(file.buffer, `${folder}/${file.originalname}`, file.mimetype)
 
     // Save document metadata in MongoDB
     const document = new Document({
